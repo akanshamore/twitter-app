543796926094be0f8164444aa9553ee9df98980b
akanshamore <akansha.more@nickelfox.com>
2024-06-08 16:45:30 +0530

    implement the google sign in function

--- frontend/src/pages/Login/SignUp.js
+++ frontend/src/pages/Login/SignUp.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import TwitterImage from "../../assets/images/1667587711730.jpeg";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import auth from "../../firebase.init"
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useSignInWithMicrosoft } from 'react-firebase-hooks/auth'
 import GoogleButton from 'react-google-button'
 import { Link } from "react-router-dom";
 import "./Login.css"
@@ -20,16 +20,24 @@ const SignUp = () => {
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
-    if (user) {
+
+
+    const [signInWithMicrosoft, googleUser, googleLoading, googleError] =
+        useSignInWithGoogle(auth
+        );
+    if (user || googleUser) {
 
         console.log(user);
+        console.log(googleUser)
     }
     if (error) {
 
         console.log(error.message);
+
     }
     if (loading) {
         console.log("loading..");
+
     }
     console.log(error);
     console.log(user);
@@ -43,6 +51,11 @@ const SignUp = () => {
         createUserWithEmailAndPassword(email, password)
     }
 
+    const handleGoogleSignIn = () => {
+
+        signInWithMicrosoft();
+    }
+
     return (
 
         <div>
@@ -52,7 +65,8 @@ const SignUp = () => {
                 </div>
                 <div className="form-container">
                     <TwitterIcon className="TwitterIcon" style={{ color: "skyblue" }} />
-                    <h2>Happening Now</h2>
+                    <h2 className="heading">Happening Now</h2>
+                    <h3 className="heading1">Join Twitter Today</h3>
 
                     <form onSubmit={handleSubmit}>
                         <input
@@ -77,7 +91,8 @@ const SignUp = () => {
                     <div className="google-button">
                         <GoogleButton
                             className="g-btn"
-                            type="light" />
+                            type="light"
+                            onClick={handleGoogleSignIn} />
                     </div>
                     <div>
                         Already have an account ?
