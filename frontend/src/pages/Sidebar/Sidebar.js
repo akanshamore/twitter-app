09b810ac5219e51d7fccdf80adabd0b7d00c7f10
akanshamore <akansha.more@nickelfox.com>
2024-06-12 17:51:58 +0530

    style the sidebar

--- frontend/src/pages/Sidebar/Sidebar.js
+++ frontend/src/pages/Sidebar/Sidebar.js
@@ -1,9 +1,9 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './Sidebar.css';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import SidebarOptions from "./SidebarOptions";
-import { Icon } from "@mui/material";
+import { Icon, IconButton, ListItemIcon, Menu, MenuItem } from "@mui/material";
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsIcon from '@mui/icons-material/Notifications';
@@ -15,9 +15,22 @@ import MoreIcon from '@mui/icons-material/More';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import DoneIcon from '@mui/icons-material/Done';
 import ListIcon from '@mui/icons-material/List';
+import { Avatar, Button } from '@mui/material';
 
 const Sidebar = () => {
+    const [anchorE1, setAnchorE1] = useState(null);
+    const openMenu = Boolean(anchorE1);
 
+    const handleClick = e => {
+
+
+        setAnchorE1(e.currentTarget);
+    }
+
+    const handleClose = () => {
+        setAnchorE1(null);
+
+    }
     return (
 
         <div className="sidebar">
@@ -30,10 +43,33 @@ const Sidebar = () => {
             <SidebarOptions active Icon={ListAltIcon} text='Lists' />
             <SidebarOptions active Icon={PermIdentityIcon} text='Profile' />
             <SidebarOptions active Icon={MoreIcon} text='More' />
-            <SidebarOptions active Icon={MoreHorizIcon} text='MoreHoriz' />
-
-
-        </div>
+            <Button variant="outlined" className="sidebar__tweet" fullWidth>
+                Tweet
+            </Button>
+            <div className="profile_info">
+                <Avatar src={"https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"}></Avatar>
+            </div>
+            <div className="user-info">
+                <div className="user_info-subuser_info"><h5>Akansha More</h5>
+                    <h5>@Akansha12</h5></div>
+                <ListItemIcon />
+            </div>
+            <div>
+                <IconButton size="small" sx={{ ml: 2 }}
+                    aria-controls={openMenu ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={openMenu ? 'basic-menu' : undefined}
+                    onClick={handleClick}
+                >
+                    <MoreHorizIcon />
+                </IconButton>
+                <Menu id="basic-menu" anchorEl={anchorE1} open={openMenu} onClick={handleClose} onClose={handleClose}>
+                    <MenuItem className="Profile__info1" >
+                        <Avatar src={"https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"} />
+                    </MenuItem>
+                </Menu>
+            </div>
+        </div >
     )
 }
 
