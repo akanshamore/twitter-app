eec35276d0cdee3501801c2241922eb0604c0bec
akanshamore <akanshamore21@gmail.com>
2024-06-26 10:17:45 +0530

    custom links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/Login/SignUp";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SignUp from './pages/Login/SignUp';
-import ProtectedRoute from './pages/ProtectedRoute';
-import PageLoading from './pages/PageLoading';
-import Feed from './pages/Feed/Feed';
-import Messages from './pages/Messsages/Messages';
-
-import Lists from './pages/Lists/Lists';
-import Explore from './pages/Explore/Explore';
-import Bookmarks from './pages/Bookmarks/Bookmarks';
-import Profile from './pages/Profile/Profile';
-import More from '@mui/icons-material/More';
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Explore from "./pages/Explore/Explore";
+import Feed from "./pages/Feed/Feed";
+import Messages from "./pages/Messsages/Messages";
+import Bookmarks from "./pages/Bookmarks/Bookmarks";
+import Lists from "./pages/Lists/Lists";
+import Profile from "./pages/Profile/Profile";
+import More from "./pages/More/More";
 import Notifications from "./pages/Notifications/Notifications";
-
-
 
 function App() {
   return (
-
     <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProtectedRoute> <Home /></ProtectedRoute>} >
+            <Route index element={<Feed />} />
+          </Route>
+          <Route path="/home" element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+          >
+            <Route path="feed" element={<Feed />} />
+            <Route path="explore" element={<Explore />} />
+            <Route path="notifications" element={<Notifications />} />
+            <Route path="messages" element={<Messages />} />
+            <Route path="bookmarks" element={<Bookmarks />} />
+            <Route path="lists" element={<Lists />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="more" element={<More />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
 
-      <Routes>
-        <Route path="/" element={<ProtectedRoute> <Home /></ProtectedRoute>} >
-          <Route index element={<Feed />} />
-        </Route>
-        <Route path="/home" element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-        >
-          <Route path="feed" element={<Feed />} />
-          <Route path="explore" element={<Explore />} />
-          <Route path="notifications" element={<Notifications />} />
-          <Route path="messages" element={<Messages />} />
-          <Route path="bookmarks" element={<Bookmarks />} />
-          <Route path="lists" element={<Lists />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="more" element={<More />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      </BrowserRouter>
 
     </div>
   );
